54480e05b70a7eca898c14e5aa897feafd898ced
igorg-rc <igor.gayvoronsky-rc@gmail.com>
2020-10-20 09:07:59 +0300

    first commit

---
+++ src/http.js
@@ -0,0 +1,62 @@
+/**
+ * EasyHTTP Library - library for making HTTP requests
+ * 
+ * @version 2.0.0
+ * @author Brad Travery
+ * @license MIT
+ */
+
+
+class EasyHTTP {
+
+  // Make HTTP GET Request
+  async get(url) {
+    const response = await fetch(url);
+    const resData = await response.json();
+    return resData;
+  }
+
+
+  // Make HTTP POST Request
+  async post(url, data) {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    });
+    const resData = await response.json();
+    return resData;
+  }
+
+
+  // Make HTTP PUT Request
+  async put(url, data) {
+
+    const response = await fetch(url, {
+      method: 'PUT',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    });
+    const resData = await response.json();
+    return resData;
+  }
+
+  // Make HTTP DELETE Request
+  async delete(url) {
+    const response = await fetch(url, {
+      method: 'DELETE',
+      headers: {
+        'Content-type': 'application/json'
+      }
+    });
+    const resData = await 'Resource deleted...';
+    return resData;
+  }
+
+}
+
+export const http = new EasyHTTP();
